ec87f1bc71bca65ee7b26f432ee9d39132b905cb
Oskar Kuziora <oskar.kuziora@golem.network>
2023-08-29 13:52:02 +0200

    Replacing vars in yaml fix

--- golem_ray/provider/node_provider.py
+++ golem_ray/provider/node_provider.py
@@ -57,14 +57,14 @@ class GolemNodeProvider(NodeProvider):
         raise GolemRayNodeProviderError(f"Image tag {image_tag} does not exist")
 
     def get_command_runner(
-        self,
-        log_prefix: str,
-        node_id: str,
-        auth_config: Dict[str, Any],
-        cluster_name: str,
-        process_runner: ModuleType,
-        use_internal_ip: bool,
-        docker_config: Optional[Dict[str, Any]] = None,
+            self,
+            log_prefix: str,
+            node_id: str,
+            auth_config: Dict[str, Any],
+            cluster_name: str,
+            process_runner: ModuleType,
+            use_internal_ip: bool,
+            docker_config: Optional[Dict[str, Any]] = None,
     ) -> CommandRunnerInterface:
         common_args = {
             "log_prefix": log_prefix,
@@ -100,10 +100,10 @@ class GolemNodeProvider(NodeProvider):
         self._golem_ray_client.set_node_tags(node_id, tags)
 
     def create_node(
-        self,
-        node_config: Dict[str, Any],
-        tags: Dict[str, str],
-        count: int,
+            self,
+            node_config: Dict[str, Any],
+            tags: Dict[str, str],
+            count: int,
     ) -> Dict[NodeId, Node]:
         return self._golem_ray_client.create_nodes(
             count=count,
@@ -129,7 +129,7 @@ class GolemNodeProvider(NodeProvider):
         def replace_placeholders(obj):
             if isinstance(obj, str):
                 obj = obj.replace("$GCS_REVERSE_TUNNEL_PORT", str(GCS_REVERSE_TUNNEL_PORT))
-                obj.replace("$RAY_HEAD_IP", str(ip_address))
+                obj = obj.replace("$RAY_HEAD_IP", str(ip_address))
                 return obj
             elif isinstance(obj, list):
                 return [replace_placeholders(item) for item in obj]
@@ -138,5 +138,6 @@ class GolemNodeProvider(NodeProvider):
             else:
                 return obj
 
+        final_config = replace_placeholders(cluster_config)
         # cluster_config.
-        return replace_placeholders(cluster_config)
+        return final_config
