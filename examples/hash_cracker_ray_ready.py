081264d5e82f367af2ce400eadd88925dc3423db
shadeofblue <blue@golem.network>
2023-09-20 16:03:33 +0200

    add comments

--- examples/hash_cracker_ray_ready.py
+++ examples/hash_cracker_ray_ready.py
@@ -5,12 +5,16 @@ from datetime import datetime
 from hashlib import sha256
 from typing import Optional
 
-# '9Lm!': de6c0da53ac2bf2b6954e400767106011e4471db7a412cce0388e3441e0ad2ec
-# `test`: 9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08
-# `foo`:  2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
-# `glm`:  2bf548d8056029c73e6e28132d19a3a277a49daf32b1c1ba7b0b7fc7e78bf5cd
-# `be`:   46599c5bb5c33101f80cea8438e2228085513dbbb19b2f5ce97bd68494d3344d
-# `x`:    2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
+# some sample phrases to crack
+#
+# 'G0!3m':   070063ad89872aaa0fc0a2f170be5641e9c5d201d25b76703a4be3ee3848016c
+# 'golem':   4c5cddb7859b93eebf26c551518c021a31fa0013b2c03afa5b541cbc8bd079a6
+# '9Lm!':    de6c0da53ac2bf2b6954e400767106011e4471db7a412cce0388e3441e0ad2ec
+# `test`:    9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08
+# `foo`:     2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
+# `glm`:     2bf548d8056029c73e6e28132d19a3a277a49daf32b1c1ba7b0b7fc7e78bf5cd
+# `be`:      46599c5bb5c33101f80cea8438e2228085513dbbb19b2f5ce97bd68494d3344d
+# `x`:       2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 
 # the character table that we want to use to construct possible phrases
 CHARS = [
@@ -73,8 +77,9 @@ def int_to_str(intval: int, round_nulls=False) -> Optional[str]:
     :param intval: the integer value to convert
     :param round_nulls: whether to round a "number" containing null values to the closest proper string.
         if set to False, `int_to_str` will just return a `None`
-    :return: the resultant string
+    :return: the resultant string, or `None` if the string contains "zeros" (in base len[CHARS])
     """
+
     div = intval
     output = ""
     base = len(CHARS) + 1
@@ -91,7 +96,22 @@ def int_to_str(intval: int, round_nulls=False) -> Optional[str]:
     return output
 
 
-def scan_range(searched_hash: str, start: int, end: int):
+def scan_range(searched_hash: str, start: int, end: int) -> Optional[str]:
+    """
+    scan a specific range of the word space for a matching hash
+
+    the extents of the range are specified as integers which correspond to numbers,
+    the digits of which are characters from the CHARS table,
+    with a base equal to the length of the CHARS table.
+
+    that way, we can trivially divide a wider range into chunks of specific size.
+
+    :param searched_hash: the hash, for which we want to find a matching word
+    :param start: searched range start
+    :param end: searched range end
+    :return: a matching word, or `None` if no match was found within the range
+    """
+
     print(f"scanning: {searched_hash}: {int_to_str(start, True)}, {int_to_str(end, True)}",)
     for i in range(start, end):
         word = int_to_str(i)
@@ -103,12 +123,24 @@ def scan_range(searched_hash: str, start: int, end: int):
 
 result = None
 
+# we start with a string consisting of a single, first character from the character set
+# e.g. `a`
 start_space = str_to_int(CHARS[0])
+
+# we traverse until a string consisting of the last character from the set, repeated `length` times
+# since a range doesn't contain its upper extent, we specify the next "number"
+# which is a string consisting of `length`+1 of the first character
+# e.g. `aaaa` when the length if equal to 3
 end_space = str_to_int(CHARS[0] * (args.length + 1))
+
+# round the chunk size up so that at most the specified number of chunks
+# is required to cover the whole range
 chunk_size = math.ceil((end_space - start_space) / args.num_chunks)
 
 for c in range(0, args.num_chunks):
-    start_chunk = start_space + c * chunk_size
+    # we don't want to search beyond the end of the range,
+    # so we clamp the start/end values to the whole range's bounds
+    start_chunk = min(end_space, start_space + c * chunk_size)
     end_chunk = min(end_space, start_chunk + chunk_size)
     chunk_result = scan_range(args.hash, start_chunk, end_chunk)
     if chunk_result:
