f42caf38a838979a1e2dda221b99ae9ae6d2fcdd
shadeofblue <blue@golem.network>
2023-09-28 18:30:41 +0200

    enable debug logging and proper startup failure reporting for the webserver

--- ray_on_golem/provider/node_provider.py
+++ ray_on_golem/provider/node_provider.py
@@ -37,6 +37,7 @@ class GolemNodeProvider(NodeProvider):
         self._ray_on_golem_client = self._get_ray_on_golem_client_instance(
             provider_parameters["webserver_port"],
             provider_parameters["enable_registry_stats"],
+            provider_parameters.get("log_level", "info"),
         )
         self._ray_on_golem_client.get_running_or_create_cluster(
             network=provider_parameters["network"],
@@ -59,6 +60,7 @@ class GolemNodeProvider(NodeProvider):
         ray_on_golem_client = cls._get_ray_on_golem_client_instance(
             provider_parameters["webserver_port"],
             provider_parameters["enable_registry_stats"],
+            provider_parameters.get("log_level", "info"),
         )
 
         auth: Dict = config["auth"]
@@ -91,11 +93,11 @@ class GolemNodeProvider(NodeProvider):
 
     @classmethod
     @lru_cache()
-    def _get_ray_on_golem_client_instance(cls, webserver_port: int, enable_registry_stats: bool):
+    def _get_ray_on_golem_client_instance(cls, webserver_port: int, enable_registry_stats: bool, log_level: str = "info"):
         ray_on_golem_client = RayOnGolemClient(webserver_port)
 
         if not is_running_on_golem_network():
-            cls._start_webserver(ray_on_golem_client, webserver_port, enable_registry_stats)
+            cls._start_webserver(ray_on_golem_client, webserver_port, enable_registry_stats, log_level)
 
         return ray_on_golem_client
 
@@ -168,7 +170,7 @@ class GolemNodeProvider(NodeProvider):
 
     @staticmethod
     def _start_webserver(
-        ray_on_golem_client: RayOnGolemClient, port: int, registry_stats: bool
+        ray_on_golem_client: RayOnGolemClient, port: int, registry_stats: bool, log_level: str,
     ) -> None:
         with cli_logger.group(WEBSERVER_LOG_GROUP):
             if ray_on_golem_client.is_webserver_running():
@@ -176,24 +178,37 @@ class GolemNodeProvider(NodeProvider):
                 return
 
             cli_logger.print("Starting webserver...")
+            args = [
+                RAY_ON_GOLEM_PATH,
+                "-p",
+                str(port),
+                "--registry-stats" if registry_stats else "--no-registry-stats",
+                "--self-shutdown",
+                "--log-level",
+                log_level,
+            ]
 
-            subprocess.Popen(
-                [
-                    RAY_ON_GOLEM_PATH,
-                    "-p",
-                    str(port),
-                    "--registry-stats" if registry_stats else "--no-registry-stats",
-                    "--self-shutdown",
-                ],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
+            cli_logger.verbose(f"Webserver command: `{' '.join([str(a) for a in args])}`")
+
+            proc = subprocess.Popen(
+                args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
                 start_new_session=True,
             )
 
             start_deadline = datetime.now() + RAY_ON_GOLEM_START_DEADLINE
             check_seconds = int(RAY_ON_GOLEM_CHECK_DEADLINE.total_seconds())
             while datetime.now() < start_deadline:
-                sleep(check_seconds)
+                try:
+                    out, err = proc.communicate(timeout=check_seconds)
+                    cli_logger.abort(
+                        "Starting webserver failed!\n"
+                        f"{out.decode('ascii')}\n"
+                        f"{err.decode('ascii')}"
+                    )
+                except subprocess.TimeoutExpired:
+                    pass
 
                 if ray_on_golem_client.is_webserver_running():
                     cli_logger.print("Starting webserver done")
