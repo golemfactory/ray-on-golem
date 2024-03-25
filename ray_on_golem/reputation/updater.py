2b418925350cbcc298fc41d7fc057be7d41c6a75
shadeofblue <blue@wave460.net>
2024-03-25 21:06:59 +0100

    apply @lucekdudek's suggestions

--- ray_on_golem/reputation/updater.py
+++ ray_on_golem/reputation/updater.py
@@ -14,6 +14,7 @@ class ReputationUpdaterException(Exception):
 
 
 class ReputationUpdater:
+    """Simple service updating the reputation data from the remote Reputation System."""
     def __init__(self, network: str = "polygon"):
         self._network = network
 
@@ -23,55 +24,63 @@ class ReputationUpdater:
 
     @contextmanager
     def _no_progress_bar(self, iterable: Iterable):
+        """emulate click's progress bar interface without an actual progress bar."""
         yield iterable
 
-    async def update(self, progress_bar=None) -> Tuple[int, int, int, int]:
-        if not progress_bar:
-            progress_bar = self._no_progress_bar
-        cnt_added = 0
-        cnt_updated = 0
-        cnt_ignored = 0
-        cnt_total = 0
+    async def get_reputation_data(self):
+        """Download the reputation data from the Reputation System endpoint."""
         async with aiohttp.request("get", self.reputation_uri) as response:
             if not response.status == 200:
                 raise ReputationUpdaterException(
                     f"Error {response.status} while updating reputation from {self.reputation_uri}"
                 )
 
-            data = await response.json()
-            with progress_bar(data.get("providers")) as providers:
-                for pdata in providers:
-                    cnt_total += 1
-                    provider_id = pdata.get("providerId")
-                    scores = pdata.get("scores")
-                    if not (provider_id and scores):
-                        cnt_ignored += 1
-                    else:
-                        network, _ = await m.Network.get_or_create(network_name=self._network)
-                        node, _ = await m.Node.get_or_create(node_id=provider_id)
-                        node_reputation, created = await m.NodeReputation.get_or_create(
-                            node=node, network=network
-                        )
+            return await response.json()
 
-                        updated = False
-                        success_rate = scores.get("successRate")
-                        uptime = scores.get("uptime")
-                        if (
-                            success_rate is not None
-                            and node_reputation.success_rate != success_rate
-                        ):
-                            node_reputation.success_rate = success_rate
-                            updated = True
-                        if uptime is not None and node_reputation.uptime != uptime:
-                            node_reputation.uptime = uptime
-                            updated = True
+    async def update(self, progress_bar=None) -> Tuple[int, int, int, int]:
+        """Perform the update."""
+        if not progress_bar:
+            progress_bar = self._no_progress_bar
 
-                        if updated:
-                            await node_reputation.save()
+        data = await self.get_reputation_data()
 
-                        if created:
-                            cnt_added += 1
-                        elif updated:
-                            cnt_updated += 1
+        cnt_added = cnt_updated = cnt_ignored = cnt_total = 0
+
+        with progress_bar(data.get("providers")) as providers:
+            for pdata in providers:
+                cnt_total += 1
+                provider_id = pdata.get("providerId")
+                scores = pdata.get("scores")
+
+                if not (provider_id and scores):
+                    cnt_ignored += 1
+                    continue
+
+                network, _ = await m.Network.get_or_create(network_name=self._network)
+                node, _ = await m.Node.get_or_create(node_id=provider_id)
+                node_reputation, created = await m.NodeReputation.get_or_create(
+                    node=node, network=network
+                )
+
+                updated = False
+                success_rate = scores.get("successRate")
+                uptime = scores.get("uptime")
+                if (
+                    success_rate is not None
+                    and node_reputation.success_rate != success_rate
+                ):
+                    node_reputation.success_rate = success_rate
+                    updated = True
+                if uptime is not None and node_reputation.uptime != uptime:
+                    node_reputation.uptime = uptime
+                    updated = True
+
+                if updated:
+                    await node_reputation.save()
+
+                if created:
+                    cnt_added += 1
+                elif updated:
+                    cnt_updated += 1
 
         return cnt_added, cnt_updated, cnt_ignored, cnt_total
